ea3d371c44e902a93135adfcf665ba8056f675f5
Pukitis Valters <pvalters.mmt-b2015@fh-salzburg.ac.at>
2016-11-11 18:21:05 +0100

    01_final

--- 01-modules/src/module.js
+++ 01-modules/src/module.js
@@ -1,15 +1,21 @@
 export function valid(email) {
-
+    var regEx = /[0-9]{4}@fh-salzburg.ac.at/;
+    return regEx.test(email);
 }
 
 export function degreeProgram(email) {
-
+    var regEx = /[a-z]{3,4}(?=-[bm][0-9]{4}@)/;
+    return regEx.exec(email)[0].toUpperCase();
 }
 
 export function level(email) {
-
+    var regEx = /[a-z](?=[0-9]{4}@)/;
+    return regEx.exec(email)[0].toUpperCase() + 'A';
 }
 
 export function graduationYear(email) {
+    var regEx = /[0-9]{4}(?=@)/;
+    var year = parseInt(regEx.exec(email)[0]);
 
+    return year + (level(email) == 'BA' ? 3 : 2);
 }
