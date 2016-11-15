9a317fb8f63a3a24fcda47f4ec091cf94e8198af
Pukitis Valters <pvalters.mmt-b2015@fh-salzburg.ac.at>
2016-11-15 22:55:48 +0100

    final router

--- 02-router/src/app.js
+++ 02-router/src/app.js
@@ -1,24 +1,62 @@
-import $ from 'jquery'
-import router from './router'
-import homeTpl from './templates/home.hbs'
-import contactTpl from './templates/contact.hbs'
-import notFoundTpl from './templates/not-found.hbs'
+import $ from 'jquery';
+import {router, goto} from './router';
+import homeTpl from './templates/home.hbs';
+import contactTpl from './templates/contact.hbs';
+import notFoundTpl from './templates/not-found.hbs';
+import magnusTpl from './templates/magnus.hbs';
+import sergeyTpl from './templates/sergey.hbs';
 
-const $app = $('#app')
+
+
+$('a').on('click', function(){
+  let url = $(this).attr('href');
+
+  if($(this).attr('rel') != "download" && $(this).attr('rel') != "external"){
+    goto(url);
+    return false;
+  }
+
+})
+
+const $app = $('#app');
 
 function index() {
-  $app.html(homeTpl())
+  $app.html(homeTpl());
 }
 
 function contact() {
-  $app.html(contactTpl())
+  $app.html(contactTpl());
+}
+
+function player(partString){
+  switch(partString){
+    case "magnus":
+      $app.html(magnusTpl());
+      break;
+    case "sergey":
+      $app.html(sergeyTpl());
+      break;
+  }
 }
 
 function notFound() {
-  $app.html(notFoundTpl())
+  $app.html(notFoundTpl());
 }
 
-router('/', index)
-router('/contact', contact)
-router('*', notFound)
-router()
+
+router('/contact', contact);
+router('/players/:player', player);
+router('/players/:player/test', player);
+router('/', index);
+router('*', notFound);
+
+
+$(document).ready(goto(window.location.pathname))
+
+$(window).bind('popstate', function(e){
+  e.preventDefault();
+  if(e.state !== null){
+    goto(e.originalEvent.state)
+  }
+
+})
